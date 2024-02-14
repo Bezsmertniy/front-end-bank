a124fadb91c1342a8a803da3c9af8348f893eda4
Bezsmertniy <bezsmertnyi-a@ukr.net>
2024-02-14 16:12:08 +0200

    help

---
+++ front/src/component/status_bar/index.js
@@ -0,0 +1,31 @@
+import "./index.css";
+import React, { useState, useEffect } from "react";
+import cellImage from "./media/cell.svg";
+import wifiImage from "./media/wifi.svg";
+import batImage from "./media/bat.svg";
+
+export default function STATUS_BAR() {
+  const [currentTime, setCurrentTime] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const date = new Date();
+      const hours = date.getHours().toString().padStart(2, "0");
+      const minutes = date.getMinutes().toString().padStart(2, "0");
+      setCurrentTime(`${hours}:${minutes}`);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <header className="status_bar">
+      <div>{currentTime}</div>
+      <span>
+        <img src={cellImage} alt="Cellular icon" />
+        <img src={wifiImage} alt="Wi-Fi icon" />
+        <img src={batImage} alt="Battery icon" />
+      </span>
+    </header>
+  );
+}
