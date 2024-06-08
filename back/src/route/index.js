9195cfce6a2bc1d8be0aebf098e103dad2060fbb
Bezsmertniy <bezsmertnyi-a@ukr.net>
2024-06-08 13:27:13 +0300

    homework

--- back/src/route/index.js
+++ back/src/route/index.js
@@ -2,17 +2,7 @@
 const express = require('express')
 const router = express.Router()
 
-// Підключіть файли роутів
-// const test = require('./test')
-// Підключіть інші файли роутів, якщо є
-
-// Об'єднайте файли роутів за потреби
-// router.use('/', test)
-// Використовуйте інші файли роутів, якщо є
-
-router.get('/', (req, res) => {
-  res.status(200).json('Hello World')
-})
+const users = []
 
 // Експортуємо глобальний роутер
 module.exports = router
